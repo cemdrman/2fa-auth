2b3d85153c10cee051b36b704e50338461da2125
Cem Dırman <cemdrman@gmail.com>
2020-12-27 15:26:19 +0300

    user entity

--- src/main/java/com/bilisimio/model/User.java
+++ src/main/java/com/bilisimio/model/User.java
@@ -1,5 +1,10 @@
 package com.bilisimio.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +12,21 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "Users")
 public class User {
-	
+
+	@Id
 	private Long id;
+	@Column
 	private String name;
+	@Column
 	private String lastName;
+	@Column
 	private String password;
+	@Column
 	private String email;
+	@Column
 	private String phone;
 
 }
