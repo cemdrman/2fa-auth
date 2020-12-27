3191dfcae6a473ffc11d204faf14eb1df5aa11cd
Cem Dırman <cemdrman@gmail.com>
2020-12-27 14:09:04 +0300

    userInput class

---
+++ src/main/java/com/bilisimio/input/UserInput.java
@@ -0,0 +1,15 @@
+package com.bilisimio.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInput {
+
+	private String email;
+	private String password;
+
+}
