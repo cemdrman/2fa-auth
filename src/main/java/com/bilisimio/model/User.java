8b4bc51a591e252af47b3c7562daa19ad9af8155
Cem Dırman <cemdrman@gmail.com>
2020-12-29 01:06:48 +0300

    modified

--- src/main/java/com/bilisimio/model/User.java
+++ src/main/java/com/bilisimio/model/User.java
@@ -7,12 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "User")
@@ -27,7 +25,7 @@ public class User {
 	private String lastName;
 	@Column
 	private String password;
-	@Column
+	@Column(unique = true)
 	private String email;
 	@Column
 	private String phone;
